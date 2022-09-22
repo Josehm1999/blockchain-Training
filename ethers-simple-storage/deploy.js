2711c84a96d613eb646e0697c89e18286672502f
josehm <josehm1999@gmail.com>
2022-09-22 17:12:47 -0500

    Primer despligue de smart contract en ambiente local
    commit 1b3d8bcefb1d209dd447d267eb3eafc5bea495cd
    Author: JoseH <josehm1999@gmail.com>
    Date:   Tue Sep 20 06:56:15 2022 +0000
    
        Se añadio gitignore
    
    commit b79987544d6fabeb29a25f90eef743ef84c98de2
    Author: JoseH <josehm1999@gmail.com>
    Date:   Tue Sep 20 05:01:55 2022 +0000
    
        First commit

--- ethers-simple-storage/deploy.js
+++ ethers-simple-storage/deploy.js
@@ -1,12 +1,30 @@
-function hi() {
-  console.log('hi')
-  let variable = 5
-  console.log(variable)
+const ethers = require('ethers')
+const fs = require('fs-extra')
+
+async function main() {
+  const provider = new ethers.providers.JsonRpcProvider(
+    'HTTP://172.19.32.1:7545'
+  )
+  const wallet = new ethers.Wallet(
+    '48ee1ab46cda0f2a50a7807a2db51891fd47eb1d270ce52c2e00dd17b0cf9aba',
+    provider
+  )
+
+  const abi = fs.readFileSync('./SimpleStorage_sol_SimpleStorage.abi', 'utf8')
+  const binary = fs.readFileSync(
+    './SimpleStorage_sol_SimpleStorage.bin',
+    'utf8'
+  )
+
+  const contractFactory = new ethers.ContractFactory(abi, binary, wallet)
+  console.log('Deployig, please wait')
+  const contract = await contractFactory.deploy()
+  console.log(contract)
 }
 
 main()
   .then(() => process.exit(0))
-  .catch((err) => {
+  .catch((error) => {
     console.error(error)
     process.exit(1)
   })
