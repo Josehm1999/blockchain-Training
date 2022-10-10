d6277755195777edd5201670e7eb3eac13555d02
JoseH <josehm1999@gmail.com>
2022-10-10 01:02:35 -0500

    title registry project bootstrap

---
+++ hardhat-title-registry-system/contracts/TitleRegistry.sol
@@ -0,0 +1,123 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+error TitlRegistry__PriceMustBeAboveZero();
+error TitleRegistry__NotApprovedForSystem();
+error TitlRegistry__AlreadyListed(address titleAddress, uint256 tokenId);
+error TittleRegistry__NotOwner();
+error TitlRegistry__NotListed(address titleAddress, uint256 tokenId);
+error TitlRegistry__PriceNotMet(
+    address titleAddress,
+    uint256 tokenId,
+    uint256 price
+);
+
+contract TitlRegistry {
+    struct Listing {
+        uint256 price;
+        address seller;
+    }
+
+    event TitleListed(
+        address indexed seller,
+        address indexed titleAddress,
+        uint256 indexed tokenId,
+        uint256 price
+    );
+
+    // Listado de las direcciones (ethereum) de los titulos -> TokenID Titulo -> Listing
+    mapping(address => mapping(uint256 => Listing)) private s_listings;
+
+    // Direccion (etherem) del vendedor -> Cantidad recolectada
+    mapping(address => uint256) private s_proceeds;
+
+    //Modifiers
+    modifier notListed(
+        address titleAddress,
+        uint256 tokenId,
+        address owner
+    ) {
+        Listing memory listing = s_listings[titleAddress][tokenId];
+        if (listing.price > 0) {
+            revert TitlRegistry__AlreadyListed(titleAddress, tokenId);
+        }
+        _;
+    }
+
+    modifier isListed(address titleAddress, uint256 tokenId) {
+        Listing memory listing = s_listings[titleAddress][tokenId];
+
+        if (listing.price <= 0) {
+            revert TitlRegistry__NotListed(titleAddress, tokenId);
+        }
+        _;
+    }
+    modifier isOwner(
+        address titleAddress,
+        uint256 tokenId,
+        address spender
+    ) {
+        IERC721 title = IERC721(titleAddress);
+        address owner = title.ownerOf(tokenId);
+        if (spender != owner) {
+            revert TittleRegistry__NotOwner();
+        }
+        _;
+    }
+
+    // Main functions
+
+    /* @notice Metodo para listar los titulos de propiedad en el sistema
+     * @param titleAddress: Direccion (ethereum) del titulo
+     * @param tokenId: El token identificador del titulo
+     * @param price: El precio de venta del titulo de la propiedad listada
+     * @dev Tecnicamente, se podria utilizar el contrato como una cuenta de retencion para el titulo
+     * pero de esta manera el dueño puede mantener su titulo mientras se encuentra listado
+     */
+
+    function listTitle(
+        address titleAddress,
+        uint256 tokenId,
+        uint256 price
+    )
+        external
+        notListed(titleAddress, tokenId, msg.sender)
+        isOwner(titleAddress, tokenId, msg.sender)
+    {
+        if (price <= 0) {
+            revert TitlRegistry__PriceMustBeAboveZero();
+        }
+
+        // 1. Enviar el titulo de propiedad al contrato. Tranferencia -> Contrato "mantener" el titulo.
+        // 2. Los dueños pueden mantener su titulo, y dar aprovacion al sistema para la venta del titulo
+        // por ellos.
+
+        IERC721 title = IERC721(titleAddress);
+        if (title.getApproved(tokenId) != address(this)) {
+            revert TitleRegistry__NotApprovedForSystem();
+        }
+        s_listings[titleAddress][tokenId] = Listing(price, msg.sender);
+        emit TitleListed(msg.sender, titleAddress, tokenId, price);
+    }
+
+    function buyTitle(address titleAddress, uint256 tokenId)
+        external
+        payable
+        isListed(titleAddress, tokenId)
+    {
+        if (msg.value < listedItem.price) {
+            revert TitlRegistry__PriceNotMet(
+                titleAddress,
+                tokenId,
+                listedItem.price
+            );
+        }
+    }
+}
+
+// 1. `listTitle`: Listar los titulos de propiedad en el sistema de registro
+// 2. `buyTitle`: Comprar un titulo de propiedad
+// 3. `cancelTitle`: Cancela el listado de un titulo de propiedad
+// 4. `updateListing`: Actualiza la información de un titulo de propiedad
+// 5. `withdrawProceeds`: Retirar el pago cuando una venta se complete exitosamente
--- hardhat-title-registry-system/hardhat.config.ts
+++ hardhat-title-registry-system/hardhat.config.ts
@@ -6,6 +6,7 @@ import "hardhat-gas-reporter"
 import "dotenv/config"
 import "solidity-coverage"
 import "hardhat-deploy"
+import "solidity-coverage"
 import { HardhatUserConfig } from "hardhat/config"
 
 /**
@@ -16,12 +17,11 @@ const MAINNET_RPC_URL =
     process.env.MAINNET_RPC_URL ||
     process.env.ALCHEMY_MAINNET_RPC_URL ||
     "https://eth-mainnet.alchemyapi.io/v2/your-api-key"
-const RINKEBY_RPC_URL =
-    process.env.RINKEBY_RPC_URL || "https://eth-rinkeby.alchemyapi.io/v2/your-api-key"
-const KOVAN_RPC_URL = process.env.KOVAN_RPC_URL || "https://eth-kovan.alchemyapi.io/v2/your-api-key"
+const GOERLI_RPC_URL =
+    process.env.GOERLI_RPC_URL || "https://eth-goerli.alchemyapi.io/v2/your-api-key"
 const POLYGON_MAINNET_RPC_URL =
     process.env.POLYGON_MAINNET_RPC_URL || "https://polygon-mainnet.alchemyapi.io/v2/your-api-key"
-const PRIVATE_KEY = process.env.PRIVATE_KEY
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0x"
 // optional
 const MNEMONIC = process.env.MNEMONIC || "your mnemonic"
 
@@ -30,7 +30,7 @@ const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "Your etherscan API k
 const POLYGONSCAN_API_KEY = process.env.POLYGONSCAN_API_KEY || "Your polygonscan API key"
 const REPORT_GAS = process.env.REPORT_GAS || false
 
-const config: HardhatUserConfig = {
+module.exports = {
     defaultNetwork: "hardhat",
     networks: {
         hardhat: {
@@ -43,23 +43,14 @@ const config: HardhatUserConfig = {
         localhost: {
             chainId: 31337,
         },
-        kovan: {
-            url: KOVAN_RPC_URL,
-            accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
-            //accounts: {
-            //     mnemonic: MNEMONIC,
-            // },
-            saveDeployments: true,
-            chainId: 42,
-        },
-        rinkeby: {
-            url: RINKEBY_RPC_URL,
+        goerli: {
+            url: GOERLI_RPC_URL,
             accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
             //   accounts: {
             //     mnemonic: MNEMONIC,
             //   },
             saveDeployments: true,
-            chainId: 4,
+            chainId: 5,
         },
         mainnet: {
             url: MAINNET_RPC_URL,
@@ -80,17 +71,20 @@ const config: HardhatUserConfig = {
     etherscan: {
         // npx hardhat verify --network <NETWORK> <CONTRACT_ADDRESS> <CONSTRUCTOR_PARAMETERS>
         apiKey: {
-            rinkeby: ETHERSCAN_API_KEY,
-            kovan: ETHERSCAN_API_KEY,
+            goerli: ETHERSCAN_API_KEY,
             polygon: POLYGONSCAN_API_KEY,
         },
     },
     gasReporter: {
-        enabled: false,
-        currency: "USD",
+        enabled: REPORT_GAS,
+        currency: "PEN",
         outputFile: "gas-report.txt",
         noColors: true,
         // coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+    },
+    contractSizer: {
+        runOnCompile: false,
+        only: ["TitleRegistry"],
     },
     namedAccounts: {
         deployer: {
@@ -104,7 +98,7 @@ const config: HardhatUserConfig = {
     solidity: {
         compilers: [
             {
-                version: "0.8.8",
+                version: "0.8.7",
             },
             {
                 version: "0.4.24",
@@ -115,5 +109,3 @@ const config: HardhatUserConfig = {
         timeout: 200000, // 200 seconds max for running tests
     },
 }
-
-export default config
